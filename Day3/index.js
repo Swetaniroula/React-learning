1473c275bb6d727d5c796b8e4d62655ff8efe36c
Swetaniroula <swetaniroula689@gmail.com>
2025-03-13 15:24:10 +0545

    Day 3 of js

--- Day3/index.js
+++ Day3/index.js
@@ -1,10 +1,83 @@
+//node --watch ./file ko name lekhana 
+
 // let a = 3;
 
 // console.log(a%2==0?"is even":"is odd");
 
-let arr = [1,2,3,4,5];
+// let arr = [1,2,3,4,5];
 
-const num = arr.map(arr=> arr*arr);
+// const num = arr.map((arr=> arr*arr));
 
-console.log(num);
- 
+// console.log(num);
+
+// function sayHello(){
+//     return "Hello world"
+// }
+// console.log(sayHello());
+
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(20,5));
+
+// function user(name="sweta"){
+//     return `hello ${name}`;
+// }
+// console.log(user());
+
+
+//arrow Function
+// const addd = ({num1,num2}) => num1/num2;  //destructuring
+//  console.log(addd({num1:25 ,num2:1}));
+
+// const add = (a,b)=> a/b;
+// console.log(add(20,1));
+
+// const User = (name="guest") => `Hello ${name}`
+// console.log(User());
+
+// let i =3;
+//  while(i <=5){
+//     console.log(i);
+//     i++;
+//  }
+
+// let j=3;
+// do{
+//     console.log(j);
+//     j++;
+// } while(j <=5)
+
+let num =1;
+for(num=1;num<=10;num++){
+    console.log(num);
+}
+
+console.log("....")
+
+let num2 = 10;
+ while(num2>=1){
+    console.log(num2);
+    num2--;
+}
+
+console.log("....")
+
+let a = 1;
+do{
+    console.log(a);
+    a++;
+}while(a<=5);
+
+ console.log("....")
+
+let Colors=["Red","Blue","Green"];
+Colors.forEach((n) => {
+    console.log(n);
+});
+
+
+console.log("....")
+
+
+
